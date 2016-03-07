b088566a7e522d72aafaac87be427a19e80ca32d
Rosie Bloxsom <rosie.bloxsom@gmail.com>
2016-03-07 07:26:52 +0000

    Make generic json type matcher use new IsOfType method
    Signed-off-by: Gareth Smith <gareth@totherme.org>

--- matchers/json_type_matcher.go
+++ matchers/json_type_matcher.go
@@ -27,47 +27,27 @@ func (m JSONTypeMatcher) Match(actual interface{}) (success bool, err error) {
 	if reflect.TypeOf(actual) != reflect.TypeOf(nosj.JSON{}) {
 		return false, fmt.Errorf("actual is not a JSON -- actually of type %s", reflect.TypeOf(actual))
 	}
+
 	json := actual.(nosj.JSON)
 
-	switch m.typ {
-	case nosj.JSONString:
-		return json.IsString(), nil
-	case nosj.JSONNum:
-		return json.IsNum(), nil
-	case nosj.JSONOb:
-		return json.IsOb(), nil
-	case nosj.JSONList:
-		return json.IsList(), nil
-	case nosj.JSONNull:
-		return json.IsNull(), nil
-	case nosj.JSONBool:
-		return json.IsBool(), nil
-	}
-	return false, fmt.Errorf("this is some kind of crazy JSON")
+	return json.IsOfType(m.typ), nil
 }
+
 func (m JSONTypeMatcher) FailureMessage(actual interface{}) (message string) {
 	if reflect.TypeOf(actual) != reflect.TypeOf(nosj.JSON{}) {
 		return fmt.Sprintf("expected a JSON object -- got a %s", reflect.TypeOf(actual))
 	}
 
 	json := actual.(nosj.JSON)
-	if json.IsBool() {
-		return fmt.Sprintf("expected a JSON %s -- got a JSON bool", m.typ)
-	}
-	if json.IsString() {
-		return fmt.Sprintf("expected a JSON %s -- got a JSON string", m.typ)
-	}
-	if json.IsNum() {
-		return fmt.Sprintf("expected a JSON %s -- got a JSON number", m.typ)
-	}
-	if json.IsList() {
-		return fmt.Sprintf("expected a JSON %s -- got a JSON list", m.typ)
-	}
-	if json.IsNull() {
-		return fmt.Sprintf("expected a JSON %s -- got JSON null", m.typ)
-	}
-	if json.IsOb() {
-		return fmt.Sprintf("expected a JSON %s -- got JSON object", m.typ)
+	for _, t := range []string{nosj.JSONBool,
+		nosj.JSONString,
+		nosj.JSONNum,
+		nosj.JSONList,
+		nosj.JSONNull,
+		nosj.JSONOb} {
+		if json.IsOfType(t) {
+			return fmt.Sprintf("expected a JSON %s -- got a JSON %s", m.typ, t)
+		}
 	}
 
 	return fmt.Sprintf("expected a JSON %s -- got some other crazy kind of JSON", m.typ)
