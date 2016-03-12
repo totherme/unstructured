33c157f4a409c12d74b8bd8c9a70ea535add703d
Rosie Bloxsom <rosie.bloxsom@gmail.com>
2016-03-12 17:34:05 +0000

    Add docstrings for the gnosj matchers
    Signed-off-by: Gareth Smith <gareth@totherme.org>

--- gnosj/have_key_matcher.go
+++ gnosj/have_key_matcher.go
@@ -6,14 +6,20 @@ import (
 	"github.com/totherme/nosj"
 )
 
+// HaveJSONKeyMatcher is a gomega matcher which tests if a given value
+// represents a json object containing a particular key.
 type HaveJSONKeyMatcher struct {
 	key string
 }
 
+// HaveJSONKey returns a gomega matcher  which tests if a given value
+// represents a json object containing a given `key`.
 func HaveJSONKey(key string) HaveJSONKeyMatcher {
 	return HaveJSONKeyMatcher{key: key}
 }
 
+// Match is the gomega function that actually checks if the given value
+// represents a json object containing the particular key.
 func (m HaveJSONKeyMatcher) Match(actual interface{}) (bool, error) {
 	switch j := actual.(type) {
 	default:
@@ -23,6 +29,9 @@ func (m HaveJSONKeyMatcher) Match(actual interface{}) (bool, error) {
 	}
 }
 
+// FailureMessage constructs a hopefully-helpful error message in the case that
+// the given value does not represent a json object containing the particular
+// key.
 func (m HaveJSONKeyMatcher) FailureMessage(actual interface{}) (message string) {
 	actualString := fmt.Sprintf("%+v", actual)
 	return fmt.Sprintf("expected '%s' to be a nosj.JSON object with key '%s'",
@@ -30,6 +39,9 @@ func (m HaveJSONKeyMatcher) FailureMessage(actual interface{}) (message string) 
 		m.key)
 }
 
+// NegatedFailureMessage constructs a hopefully-helpful error message in the
+// case that the given value unexpectedly represents a json object containing
+// the particular key.
 func (m HaveJSONKeyMatcher) NegatedFailureMessage(actual interface{}) (message string) {
 	actualString := fmt.Sprintf("%+v", actual)
 	return fmt.Sprintf("expected '%s' not to contain the key '%s'",
--- gnosj/json_type_matcher.go
+++ gnosj/json_type_matcher.go
@@ -1,3 +1,8 @@
+// Package gnosj provides gomega matchers for using nosj with the ginkgo and
+// gomega testing libraries:
+//
+// https://github.com/onsi/gomega
+// https://github.com/onsi/ginkgo
 package gnosj
 
 import (
@@ -7,46 +12,62 @@ import (
 	"github.com/totherme/nosj"
 )
 
+// JSONTypeMatcher is a gomega matcher which tests if a given value represents
+// json data of a given type.
 type JSONTypeMatcher struct {
 	typ string
 }
 
+// BeAnObject returns a gomega matcher which tests if a given value represents
+// a json object.
 func BeAnObject() JSONTypeMatcher {
 	return JSONTypeMatcher{
 		typ: nosj.JSONOb,
 	}
 }
 
+// BeAnObject returns a gomega matcher which tests if a given value represents
+// a json string.
 func BeAString() JSONTypeMatcher {
 	return JSONTypeMatcher{
 		typ: nosj.JSONString,
 	}
 }
 
+// BeAList returns a gomega matcher which tests if a given value represents
+// a json list.
 func BeAList() JSONTypeMatcher {
 	return JSONTypeMatcher{
 		typ: nosj.JSONList,
 	}
 }
 
+// BeANum returns a gomega matcher which tests if a given value represents
+// a json num.
 func BeANum() JSONTypeMatcher {
 	return JSONTypeMatcher{
 		typ: nosj.JSONNum,
 	}
 }
 
+// BeABool returns a gomega matcher which tests if a given value represents
+// a json bool.
 func BeABool() JSONTypeMatcher {
 	return JSONTypeMatcher{
 		typ: nosj.JSONBool,
 	}
 }
 
+// BeNull returns a gomega matcher which tests if a given value represents
+// json null.
 func BeANull() JSONTypeMatcher {
 	return JSONTypeMatcher{
 		typ: nosj.JSONNull,
 	}
 }
 
+// Match is the gomega function that actually checks if the given value is of
+// the appropriate json type.
 func (m JSONTypeMatcher) Match(actual interface{}) (success bool, err error) {
 	switch json := actual.(type) {
 	default:
@@ -56,6 +77,8 @@ func (m JSONTypeMatcher) Match(actual interface{}) (success bool, err error) {
 	}
 }
 
+// FailureMessage constructs a hopefully-helpful error message in the case that
+// the given value is not of the appropriate json type.
 func (m JSONTypeMatcher) FailureMessage(actual interface{}) (message string) {
 	if reflect.TypeOf(actual) != reflect.TypeOf(nosj.JSON{}) {
 		return fmt.Sprintf("expected a JSON object -- got a %s", reflect.TypeOf(actual))
@@ -75,6 +98,9 @@ func (m JSONTypeMatcher) FailureMessage(actual interface{}) (message string) {
 
 	return fmt.Sprintf("expected a JSON %s -- got some other crazy kind of JSON", m.typ)
 }
+
+// NegatedFailureMessage constructs a hopefully-helpful error message in the
+// case that the given value is unexpectedly of the appropriate json type.
 func (m JSONTypeMatcher) NegatedFailureMessage(actual interface{}) (message string) {
 	return fmt.Sprintf("got a JSON %s, but expected not to", m.typ)
 }
